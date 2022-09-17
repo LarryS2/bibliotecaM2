80cf1a093f582ec448906ad16a0e8077d92372ec
Ficction <santiagogcan@gmail.com>
2022-09-16 19:41:12 -0500

    Solo tiene un comentario vacío

--- bibliotecaCompartida/src/logico/ControladorUsuario.java
+++ bibliotecaCompartida/src/logico/ControladorUsuario.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 public class ControladorUsuario {
     
     public static ArrayList<Persona> listausuarios = new ArrayList<>();    
-
+    //
     public ControladorUsuario() {
         Bibliotecario me = new Bibliotecario( "0105449649", "ADMINISTRADOR");
         Persona nuevo = new Persona( "0105449641");
